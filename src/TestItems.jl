66aaf551deea4c5e98d3a638db14d5c529de20e9
Jacob Quinn <quinn.jacobd@gmail.com>
2023-01-12 16:28:53 -0700

    Add a new at-testsetup macro for defining a module that can be shared among test items

--- src/TestItems.jl
+++ src/TestItems.jl
@@ -1,9 +1,13 @@
 module TestItems
 
-export @testitem
+export @testitem, @testsetup
 
 macro testitem(ex...)
     return nothing
+end
+
+macro testsetup(name, code)
+    return Expr(:toplevel, Expr(:module, true, esc(name), esc(code)))
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,18 @@
 using TestItems
 using Test
 
+@testsetup TestSetup begin
+    const x = 10
+    getfloat() = rand()
+end
+
 @testset "TestItems" begin
 
     x = @testitem "Name of the test item" begin
 
     end
 
-    @test x == nothing
-
+    @test x === nothing
+    @test TestSetup.x === 10
+    @test TestSetup.getfloat() isa Float64
 end
