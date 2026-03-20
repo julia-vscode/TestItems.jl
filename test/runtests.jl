6d8a6e63dc44d15c48f8dd00ff5d2c5ff48cbd11
David Anthoff <anthoff@berkeley.edu>
2026-03-20 14:42:59 -0700

    Migrate tests to test item framework

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,35 @@
-using TestItems
-using Test
+using TestItemRunner
 
-@testset "TestItems" begin
+@testitem "Test @testitem" begin
 
     x = @testitem "Name of the test item" begin
         println("Hello world")
     end
     @test x === nothing
+end
 
+@testitem "Test @testmodule and @testsnippet" begin
     y = @testmodule Foo begin
         const x = 10
         getfloat() = rand()
     end
     @test y === nothing
+end
+
+@testitem "Test @testsnippet" begin
 
     z = @testsnippet Bar begin
         println("Hello world")
     end
     @test z === nothing
+end
 
+@testitem "Test @ccall in @testitem" begin
     w = @testitem begin
         error()
         @ccall splork
     end
     @test w === nothing
 end
+
+@run_package_tests
